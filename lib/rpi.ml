bff6564e41cd13c33c577383c331fc4af7e22810
Lucas Pluvinage <lucas@tarides.com>
2021-11-10 16:29:39 +0100

    fix warnings

--- lib/rpi.ml
+++ lib/rpi.ml
@@ -14,10 +14,10 @@ module Mem = struct
 
   type addr = nativeint
 
-  external wait : int -> unit = "ocamlrpi_barrier_wait" "noalloc"
-  external dsb : unit -> unit = "ocamlrpi_barrier_dsb" "noalloc"
-  external dmb : unit -> unit = "ocamlrpi_barrier_dmb" "noalloc"
-  external isb : unit -> unit = "ocamlrpi_barrier_isb" "noalloc"
+  external wait : int -> unit = "ocamlrpi_barrier_wait" [@@noalloc]
+  external dsb : unit -> unit = "ocamlrpi_barrier_dsb" [@@noalloc]
+  external dmb : unit -> unit = "ocamlrpi_barrier_dmb" [@@noalloc]
+  external isb : unit -> unit = "ocamlrpi_barrier_isb" [@@noalloc]
 
   let ( + ) = Nativeint.add
   let ( - ) = Nativeint.sub
@@ -27,41 +27,41 @@ module Mem = struct
 
   (* Reads *)
 
-  external get : addr -> int = "ocamlrpi_mem_get_byte" "noalloc"
-  external get_int : addr -> int = "ocamlrpi_mem_get_int" "noalloc"
+  external get : addr -> int = "ocamlrpi_mem_get_byte" [@@noalloc]
+  external get_int : addr -> int = "ocamlrpi_mem_get_int" [@@noalloc]
   external get_int32 : addr -> int32 = "ocamlrpi_mem_get_int32"
   external get_int64 : addr -> int64 = "ocamlrpi_mem_get_int64"
 
   (* Writes *)
 
   external set : addr -> int -> unit =
-    "ocamlrpi_mem_set_byte" "noalloc"
+    "ocamlrpi_mem_set_byte" [@@noalloc]
 
   external set_int : addr -> int -> unit =
-    "ocamlrpi_mem_set_int" "noalloc"
+    "ocamlrpi_mem_set_int" [@@noalloc]
 
   external set_int32 : addr -> int32 -> unit =
-    "ocamlrpi_mem_set_int32" "noalloc"
+    "ocamlrpi_mem_set_int32" [@@noalloc]
 
   external set_int64 : addr -> int64 -> unit =
-    "ocamlrpi_mem_set_int64" "noalloc"
+    "ocamlrpi_mem_set_int64" [@@noalloc]
 
   (* Masked writes *)
 
   external set_bits : addr -> bits:int -> int -> unit =
-    "ocamlrpi_mem_set_byte_bits" "noalloc"
+    "ocamlrpi_mem_set_byte_bits" [@@noalloc]
 
   external set_int_bits : addr -> bits:int -> int -> unit =
-    "ocamlrpi_mem_set_int_bits" "noalloc"
+    "ocamlrpi_mem_set_int_bits" [@@noalloc]
 
   external set_int32_bits : addr -> bits:int32 -> int32 -> unit =
-    "ocamlrpi_mem_set_int32_bits" "noalloc"
+    "ocamlrpi_mem_set_int32_bits" [@@noalloc]
 
   external set_int64_bits : addr -> bits:int64 -> int64 -> unit =
-    "ocamlrpi_mem_set_int64_bits" "noalloc"
+    "ocamlrpi_mem_set_int64_bits" [@@noalloc]
 
   external set_int32_pow : addr -> int -> unit =
-    "ocamlrpi_mem_set_int32_pow" "noalloc"
+    "ocamlrpi_mem_set_int32_pow" [@@noalloc]
 
   (* Mapping *)
 
