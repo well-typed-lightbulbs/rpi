3994398bc35b6d180cd30f837ecfc055a8e3588c
Lucas Pluvinage <lucas@tarides.com>
2021-11-15 15:44:13 +0100

    fix for formatting gpio pin variant type

--- lib/gpio.ml
+++ lib/gpio.ml
@@ -19,62 +19,12 @@ let gp_pudclk0 = Mem.(gp_base + 0x98n)
 let gp_pudclk1 = Mem.(gp_base + 0x98n)
 
 (* Pins *)
-
-type pin =
-  | P00
-  | P01
-  | P02
-  | P03
-  | P04
-  | P05
-  | P06
-  | P07
-  | P08
-  | P09
-  | P10
-  | P11
-  | P12
-  | P13
-  | P14
-  | P15
-  | P16
-  | P17
-  | P18
-  | P19
-  | P20
-  | P21
-  | P22
-  | P23
-  | P24
-  | P25
-  | P26
-  | P27
-  | P28
-  | P29
-  | P30
-  | P31
-  | P32
-  | P33
-  | P34
-  | P35
-  | P36
-  | P37
-  | P38
-  | P39
-  | P40
-  | P41
-  | P42
-  | P43
-  | P44
-  | P45
-  | P46
-  | P47
-  | P48
-  | P49
-  | P50
-  | P51
-  | P52
-  | P53
+type pin = P00 | P01 | P02 | P03 | P04 | P05 | P06 | P07 | P08 | P09 | P10 
+  | P11 | P12 | P13 | P14 | P15 | P16 | P17 | P18 | P19 | P20 | P21 | P22 | P23
+  | P24 | P25 | P26 | P27 | P28 | P29 | P30 | P31 | P32 | P33 | P34 | P35 | P36
+  | P37 | P38 | P39 | P40 | P41 | P42 | P43 | P44 | P45 | P46 | P47 | P48 | P49
+  | P50 | P51 | P52 | P53
+[@@ocamlformat "disable"]
 
 let pin_to_int : pin -> int = fun p -> Obj.magic (Obj.repr p)
 
