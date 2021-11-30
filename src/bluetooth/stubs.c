a9d058a6b10b54781d421150ed25de8fa320f62c
Lucas Pluvinage <lucas@tarides.com>
2021-11-30 23:02:52 +0100

    fixes from poc

--- src/bluetooth/stubs.c
+++ src/bluetooth/stubs.c
@@ -1,13 +1,10 @@
 #include <caml/bigarray.h>
 
-
-
-extern unsigned char _binary_BCM4345C0_hcd_start[];
-extern unsigned char _binary_BCM4345C0_hcd_size[];
+extern unsigned char _binary_BCM4345C0_hcd_start;
+extern unsigned char _binary_BCM4345C0_hcd_end;
 
 CAMLprim value caml_bt_get_firmware(value unit) {
     long dims[1];
-    dims[0] = (long)&_binary_BCM4345C0_hcd_size;
-
-    return caml_ba_alloc(CAML_BA_UINT8 | CAML_BA_C_LAYOUT, 1, _binary_BCM4345C0_hcd_start, dims);
+    dims[0] = (long)(&_binary_BCM4345C0_hcd_end - &_binary_BCM4345C0_hcd_start);
+    return caml_ba_alloc(CAML_BA_UINT8 | CAML_BA_C_LAYOUT, 1, &_binary_BCM4345C0_hcd_start, dims);
 }
